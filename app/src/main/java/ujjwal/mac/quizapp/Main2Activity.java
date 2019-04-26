541ba519ca0cbd5ddee953a735f1d6aa878b4690
Ujjwal <ujjwal1317@gmail.com>
2019-04-26 08:28:37 +0530

    first commit

---
+++ app/src/main/java/ujjwal/mac/quizapp/Main2Activity.java
@@ -0,0 +1,138 @@
+package ujjwal.mac.quizapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+public class Main2Activity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener , RadioButton.OnCheckedChangeListener{
+
+    RadioButton o1 , o2 , o3 , o4 ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        o1 = findViewById(R.id.option1);
+        o2 = findViewById(R.id.option2);
+        o3 = findViewById(R.id.option3);
+        o4 = findViewById(R.id.option4);
+        o1.setOnCheckedChangeListener(this);
+        o2.setOnCheckedChangeListener(this);
+        o3.setOnCheckedChangeListener(this);
+        o4.setOnCheckedChangeListener(this);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        if (b) {
+            switch (compoundButton.getId()) {
+                case R.id.option1:
+                    o1.setChecked(true);
+                    o2.setChecked(false);
+                    o3.setChecked(false);
+                    o4.setChecked(false);
+                    break;
+                case R.id.option2:
+                    o2.setChecked(true);
+                    o1.setChecked(false);
+                    o3.setChecked(false);
+                    o4.setChecked(false);
+                    break;
+                case R.id.option3:
+                    o3.setChecked(true);
+                    o2.setChecked(false);
+                    o1.setChecked(false);
+                    o4.setChecked(false);
+                    break;
+                case R.id.option4:
+                    o4.setChecked(true);
+                    o2.setChecked(false);
+                    o3.setChecked(false);
+                    o1.setChecked(false);
+                    break;
+            }
+        }
+    }
+}
